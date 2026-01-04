137daccf1ce33fe3f101b332ff37909fb828c3aa
SuChang <1614754563@qq.com>
2026-01-04 23:10:05 +0800

    调整样式

---
+++ NarakaBladepoint.Modules/PersonalInformation/UI/SocialAvatar/Models/AvatarItemModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace NarakaBladepoint.Modules.PersonalInformation.UI.SocialAvatar.Models
+{
+    internal class AvatarItemModel
+    {
+        public string Name { get; set; }
+        public ImageSource Icon { get; set; }
+        public string Description { get; set; }
+
+        public bool IsLocked { get; set; }
+    }
+}
--- NarakaBladepoint.Modules/PersonalInformation/UI/SocialAvatar/ViewModels/SocialAvatarPageViewModel.cs
+++ NarakaBladepoint.Modules/PersonalInformation/UI/SocialAvatar/ViewModels/SocialAvatarPageViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace NarakaBladepoint.Modules.PersonalInformation.UI.SocialAvatar.ViewModels
+﻿using System.Collections.ObjectModel;
+using NarakaBladepoint.Modules.PersonalInformation.UI.SocialAvatar.Models;
+
+namespace NarakaBladepoint.Modules.PersonalInformation.UI.SocialAvatar.ViewModels
 {
     internal class SocialAvatarPageViewModel : ViewModelBase
     {
+        public ObservableCollection<AvatarItemModel> AvatarItemModels { get; set; } = [];
+
         public SocialAvatarPageViewModel(IContainerExtension containerExtension)
             : base(containerExtension) { }
     }
