d3a61d687d05437f9b8280446a1ada0744ffc047
1614754563@qq.com <1614754563@qq.com>
2026-01-21 17:57:33 +0800

    临时提交，补充活动中心main界面ViewModel属性

--- NarakaBladepoint.Modules/EventCenter/Domain/EventParameters/NavigatedToEventCenterParameter.cs
+++ NarakaBladepoint.Modules/EventCenter/Domain/EventParameters/NavigatedToEventCenterParameter.cs
@@ -13,13 +13,85 @@ namespace NarakaBladepoint.Modules.EventCenter.Domain.EventParameters
     internal class NavigatedToEventCenterParameter : NavigationParameters
     {
         /// <summary>
+        /// 是否选中近期热点
+        /// </summary>
+        public bool IsSelectedLatestNews { get; set; }
+
+        /// <summary>
         /// 是否选中限时活动
         /// </summary>
-        public bool IsSelectedLimitedEvent { get; }
+        public bool IsSelectedLimitedEvent { get; set; }
 
-        public NavigatedToEventCenterParameter(bool IsSelectedLimitedEvent = false)
+        /// <summary>
+        /// 是否选中揽月阁
+        /// </summary>
+        public bool IsSelectedMoonGazingPavilion { get; set; }
+
+        /// <summary>
+        /// 是否选中白泽月卡
+        /// </summary>
+        public bool IsSelectedBaiZeCard { get; set; }
+
+        /// <summary>
+        /// 是否选中绑定有礼
+        /// </summary>
+        public bool IsSelectedBindReward { get; set; }
+
+        /// <summary>
+        /// 是否选中英雄归来
+        /// </summary>
+        public bool IsSelectedWelcomeBack { get; set; }
+
+        /// <summary>
+        /// 是否选中传火
+        /// </summary>
+        public bool IsSelectedPassingTheFlame { get; set; }
+
+        /// <summary>
+        /// 是否选中蓬莱指南
+        /// </summary>
+        public bool IsSelectedPenglaiGuide { get; set; }
+
+        /// <summary>
+        /// 是否选中更新公告
+        /// </summary>
+        public bool IsSelectedPatchNote { get; set; }
+
+        /// <summary>
+        /// 是否选中网易支付好礼
+        /// </summary>
+        public bool IsSelectedNetEasePayRewards { get; set; }
+
+        /// <summary>
+        /// 是否选中宝箱定向保底
+        /// </summary>
+        public bool IsSelectedTargetedChestGuarantee { get; set; }
+
+        public NavigatedToEventCenterParameter(
+            bool isSelectedLatestNews = false,
+            bool isSelectedLimitedEvent = false,
+            bool isSelectedMoonGazingPavilion = false,
+            bool isSelectedBaiZeCard = false,
+            bool isSelectedBindReward = false,
+            bool isSelectedWelcomeBack = false,
+            bool isSelectedPassingTheFlame = false,
+            bool isSelectedPenglaiGuide = false,
+            bool isSelectedPatchNote = false,
+            bool isSelectedNetEasePayRewards = false,
+            bool isSelectedTargetedChestGuarantee = false
+        )
         {
-            this.IsSelectedLimitedEvent = IsSelectedLimitedEvent;
+            this.IsSelectedLatestNews = isSelectedLatestNews;
+            this.IsSelectedLimitedEvent = isSelectedLimitedEvent;
+            this.IsSelectedMoonGazingPavilion = isSelectedMoonGazingPavilion;
+            this.IsSelectedBaiZeCard = isSelectedBaiZeCard;
+            this.IsSelectedBindReward = isSelectedBindReward;
+            this.IsSelectedWelcomeBack = isSelectedWelcomeBack;
+            this.IsSelectedPassingTheFlame = isSelectedPassingTheFlame;
+            this.IsSelectedPenglaiGuide = isSelectedPenglaiGuide;
+            this.IsSelectedPatchNote = isSelectedPatchNote;
+            this.IsSelectedNetEasePayRewards = isSelectedNetEasePayRewards;
+            this.IsSelectedTargetedChestGuarantee = isSelectedTargetedChestGuarantee;
             AddParametersToNavigationParameters();
         }
 
--- NarakaBladepoint.Modules/EventCenter/UI/Main/ViewModels/EventCenterMainPageViewModel.cs
+++ NarakaBladepoint.Modules/EventCenter/UI/Main/ViewModels/EventCenterMainPageViewModel.cs
@@ -5,6 +5,23 @@ namespace NarakaBladepoint.Modules.EventCenter.UI.Main.ViewModels
 {
     internal class EventCenterMainPageViewModel : CanRemoveHomePageRegionViewModelBase
     {
+        /// <summary>
+        /// 是否选中近期热点Tab
+        /// </summary>
+        private bool _isSelectedLatestNews = false;
+        public bool IsSelectedLatestNews
+        {
+            get { return _isSelectedLatestNews; }
+            set
+            {
+                _isSelectedLatestNews = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中限时活动Tab
+        /// </summary>
         private bool _isSelectedLimitedEvent = false;
         public bool IsSelectedLimitedEvent
         {
@@ -16,18 +33,193 @@ namespace NarakaBladepoint.Modules.EventCenter.UI.Main.ViewModels
             }
         }
 
+        /// <summary>
+        /// 是否选中揽月阁Tab
+        /// </summary>
+        private bool _isSelectedMoonGazingPavilion = false;
+        public bool IsSelectedMoonGazingPavilion
+        {
+            get { return _isSelectedMoonGazingPavilion; }
+            set
+            {
+                _isSelectedMoonGazingPavilion = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中白泽月卡Tab
+        /// </summary>
+        private bool _isSelectedBaiZeCard = false;
+        public bool IsSelectedBaiZeCard
+        {
+            get { return _isSelectedBaiZeCard; }
+            set
+            {
+                _isSelectedBaiZeCard = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中绑定有礼Tab
+        /// </summary>
+        private bool _isSelectedBindReward = false;
+        public bool IsSelectedBindReward
+        {
+            get { return _isSelectedBindReward; }
+            set
+            {
+                _isSelectedBindReward = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中英雄归来Tab
+        /// </summary>
+        private bool _isSelectedWelcomeBack = false;
+        public bool IsSelectedWelcomeBack
+        {
+            get { return _isSelectedWelcomeBack; }
+            set
+            {
+                _isSelectedWelcomeBack = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中传火Tab
+        /// </summary>
+        private bool _isSelectedPassingTheFlame = false;
+        public bool IsSelectedPassingTheFlame
+        {
+            get { return _isSelectedPassingTheFlame; }
+            set
+            {
+                _isSelectedPassingTheFlame = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中蓬莱指南Tab
+        /// </summary>
+        private bool _isSelectedPenglaiGuide = false;
+        public bool IsSelectedPenglaiGuide
+        {
+            get { return _isSelectedPenglaiGuide; }
+            set
+            {
+                _isSelectedPenglaiGuide = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中更新公告Tab
+        /// </summary>
+        private bool _isSelectedPatchNote = false;
+        public bool IsSelectedPatchNote
+        {
+            get { return _isSelectedPatchNote; }
+            set
+            {
+                _isSelectedPatchNote = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中网易支付好礼Tab
+        /// </summary>
+        private bool _isSelectedNetEasePayRewards = false;
+        public bool IsSelectedNetEasePayRewards
+        {
+            get { return _isSelectedNetEasePayRewards; }
+            set
+            {
+                _isSelectedNetEasePayRewards = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 是否选中宝箱定向保底Tab
+        /// </summary>
+        private bool _isSelectedTargetedChestGuarantee = false;
+        public bool IsSelectedTargetedChestGuarantee
+        {
+            get { return _isSelectedTargetedChestGuarantee; }
+            set
+            {
+                _isSelectedTargetedChestGuarantee = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public EventCenterMainPageViewModel() { }
 
+        /// <summary>
+        /// 当导航到此页面时执行
+        /// </summary>
         protected override void OnNavigatedToExecute(NavigationContext navigationContext)
         {
-            if (
-                navigationContext.Parameters.TryGetValue(
-                    nameof(NavigatedToEventCenterParameter.IsSelectedLimitedEvent),
-                    out bool isSelectedLimitedEventParameter
-                )
-            )
+            // 尝试从导航参数中获取各个Tab的选中状态
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedLatestNews), out bool isSelectedLatestNews))
             {
-                this.IsSelectedLimitedEvent = isSelectedLimitedEventParameter;
+                this.IsSelectedLatestNews = isSelectedLatestNews;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedLimitedEvent), out bool isSelectedLimitedEvent))
+            {
+                this.IsSelectedLimitedEvent = isSelectedLimitedEvent;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedMoonGazingPavilion), out bool isSelectedMoonGazingPavilion))
+            {
+                this.IsSelectedMoonGazingPavilion = isSelectedMoonGazingPavilion;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedBaiZeCard), out bool isSelectedBaiZeCard))
+            {
+                this.IsSelectedBaiZeCard = isSelectedBaiZeCard;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedBindReward), out bool isSelectedBindReward))
+            {
+                this.IsSelectedBindReward = isSelectedBindReward;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedWelcomeBack), out bool isSelectedWelcomeBack))
+            {
+                this.IsSelectedWelcomeBack = isSelectedWelcomeBack;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedPassingTheFlame), out bool isSelectedPassingTheFlame))
+            {
+                this.IsSelectedPassingTheFlame = isSelectedPassingTheFlame;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedPenglaiGuide), out bool isSelectedPenglaiGuide))
+            {
+                this.IsSelectedPenglaiGuide = isSelectedPenglaiGuide;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedPatchNote), out bool isSelectedPatchNote))
+            {
+                this.IsSelectedPatchNote = isSelectedPatchNote;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedNetEasePayRewards), out bool isSelectedNetEasePayRewards))
+            {
+                this.IsSelectedNetEasePayRewards = isSelectedNetEasePayRewards;
+            }
+
+            if (navigationContext.Parameters.TryGetValue(nameof(NavigatedToEventCenterParameter.IsSelectedTargetedChestGuarantee), out bool isSelectedTargetedChestGuarantee))
+            {
+                this.IsSelectedTargetedChestGuarantee = isSelectedTargetedChestGuarantee;
             }
         }
     }
