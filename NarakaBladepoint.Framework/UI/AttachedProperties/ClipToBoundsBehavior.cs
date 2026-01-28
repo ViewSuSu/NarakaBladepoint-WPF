d1342e559abda66a444c19ec1cb743b3c5a3ab23
1614754563@qq.com <1614754563@qq.com>
2026-01-28 16:35:18 +0800

    feat:补充控件悬停方法与Grid裁剪附加属性

---
+++ NarakaBladepoint.Framework/UI/AttachedProperties/ClipToBoundsBehavior.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NarakaBladepoint.Framework.UI.AttachedProperties
+{
+    public static class ClipToBoundsBehavior
+    {
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "IsEnabled",
+                typeof(bool),
+                typeof(ClipToBoundsBehavior),
+                new PropertyMetadata(false, OnIsEnabledChanged)
+            );
+
+        public static bool GetIsEnabled(DependencyObject obj) =>
+            (bool)obj.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(DependencyObject obj, bool value) =>
+            obj.SetValue(IsEnabledProperty, value);
+
+        private static void OnIsEnabledChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e
+        )
+        {
+            if (d is FrameworkElement element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    // 创建裁剪矩形
+                    var clipGeometry = new RectangleGeometry();
+                    element.Clip = clipGeometry;
+
+                    // 使用弱事件监听大小变化
+                    WeakEventManager<FrameworkElement, SizeChangedEventArgs>.AddHandler(
+                        element,
+                        "SizeChanged",
+                        OnElementSizeChanged
+                    );
+
+                    UpdateClip(element);
+                }
+                else
+                {
+                    // 移除弱事件监听
+                    WeakEventManager<FrameworkElement, SizeChangedEventArgs>.RemoveHandler(
+                        element,
+                        "SizeChanged",
+                        OnElementSizeChanged
+                    );
+
+                    element.Clip = null;
+                }
+            }
+        }
+
+        private static void OnElementSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (sender is FrameworkElement element && GetIsEnabled(element))
+            {
+                UpdateClip(element);
+            }
+        }
+
+        private static void UpdateClip(FrameworkElement element)
+        {
+            if (element.Clip is RectangleGeometry rectGeometry)
+            {
+                rectGeometry.Rect = new Rect(0, 0, element.ActualWidth, element.ActualHeight);
+
+                // 如果需要圆角，可以从父容器继承
+                if (element is Border border && border.CornerRadius != new CornerRadius(0))
+                {
+                    rectGeometry.RadiusX = border.CornerRadius.TopLeft;
+                    rectGeometry.RadiusY = border.CornerRadius.TopLeft;
+                }
+                else if (
+                    element.TemplatedParent is Border templatedBorder
+                    && templatedBorder.CornerRadius != new CornerRadius(0)
+                )
+                {
+                    // 如果是在模板中的 Border
+                    rectGeometry.RadiusX = templatedBorder.CornerRadius.TopLeft;
+                    rectGeometry.RadiusY = templatedBorder.CornerRadius.TopLeft;
+                }
+            }
+        }
+    }
+}
---
+++ NarakaBladepoint.Framework/UI/AttachedProperties/ControlHoverBehavior.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace NarakaBladepoint.Framework.UI.AttachedProperties
+{
+    public static class ControlHoverBehavior
+    {
+        // 是否启用悬停放大效果
+        public static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.RegisterAttached(
+                "IsEnabled",
+                typeof(bool),
+                typeof(ControlHoverBehavior),
+                new PropertyMetadata(false, OnIsEnabledChanged)
+            );
+
+        // 放大倍数
+        public static readonly DependencyProperty ZoomFactorProperty =
+            DependencyProperty.RegisterAttached(
+                "ZoomFactor",
+                typeof(double),
+                typeof(ControlHoverBehavior),
+                new PropertyMetadata(1.2)
+            );
+
+        public static bool GetIsEnabled(DependencyObject obj) =>
+            (bool)obj.GetValue(IsEnabledProperty);
+
+        public static void SetIsEnabled(DependencyObject obj, bool value) =>
+            obj.SetValue(IsEnabledProperty, value);
+
+        public static double GetZoomFactor(DependencyObject obj) =>
+            (double)obj.GetValue(ZoomFactorProperty);
+
+        public static void SetZoomFactor(DependencyObject obj, double value) =>
+            obj.SetValue(ZoomFactorProperty, value);
+
+        private static void OnIsEnabledChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e
+        )
+        {
+            if (d is FrameworkElement element)
+            {
+                ConfigureControl(element, (bool)e.NewValue);
+            }
+        }
+
+        private static void ConfigureControl(FrameworkElement element, bool isEnabled)
+        {
+            if (isEnabled)
+            {
+                // 确保有缩放变换
+                EnsureScaleTransform(element);
+
+                // 使用弱事件监听鼠标进入和离开
+                WeakEventManager<FrameworkElement, MouseEventArgs>.AddHandler(
+                    element,
+                    "MouseEnter",
+                    OnMouseEnter
+                );
+                WeakEventManager<FrameworkElement, MouseEventArgs>.AddHandler(
+                    element,
+                    "MouseLeave",
+                    OnMouseLeave
+                );
+            }
+            else
+            {
+                // 移除弱事件监听
+                WeakEventManager<FrameworkElement, MouseEventArgs>.RemoveHandler(
+                    element,
+                    "MouseEnter",
+                    OnMouseEnter
+                );
+                WeakEventManager<FrameworkElement, MouseEventArgs>.RemoveHandler(
+                    element,
+                    "MouseLeave",
+                    OnMouseLeave
+                );
+
+                // 恢复到原始大小
+                ResetScaleTransform(element);
+            }
+        }
+
+        private static void EnsureScaleTransform(FrameworkElement element)
+        {
+            if (element.RenderTransform is not ScaleTransform)
+            {
+                // 设置变换原点为中心
+                element.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                // 创建缩放变换
+                element.RenderTransform = new ScaleTransform(1, 1);
+            }
+        }
+
+        private static void OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is FrameworkElement element && GetIsEnabled(element))
+            {
+                ApplyScaleAnimation(element, GetZoomFactor(element));
+            }
+        }
+
+        private static void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is FrameworkElement element && GetIsEnabled(element))
+            {
+                ApplyScaleAnimation(element, 1.0);
+            }
+        }
+
+        private static void ApplyScaleAnimation(FrameworkElement element, double targetScale)
+        {
+            if (element.RenderTransform is ScaleTransform scaleTransform)
+            {
+                // 停止当前动画
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, null);
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, null);
+
+                var animation = new DoubleAnimation
+                {
+                    To = targetScale,
+                    Duration = TimeSpan.FromMilliseconds(1),
+                    EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseOut },
+                };
+
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, animation);
+            }
+        }
+
+        private static void ResetScaleTransform(FrameworkElement element)
+        {
+            if (element.RenderTransform is ScaleTransform scaleTransform)
+            {
+                // 停止动画
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, null);
+                scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, null);
+
+                // 重置到原始大小
+                scaleTransform.ScaleX = 1.0;
+                scaleTransform.ScaleY = 1.0;
+            }
+        }
+    }
+}
