c191665e6f02541a6f4148ea414cf1967f5bcabc
小窗同学 <1614754563@qq.com>
2026-02-05 11:04:11 +0800

    feat:调整SocialModule位置

--- NarakaBladepoint.Modules/Social/Module/SocialModule.cs
+++ NarakaBladepoint.Modules/Social/Module/SocialModule.cs
@@ -11,7 +11,7 @@ using NarakaBladepoint.Modules.Social.UI.Social.Views;
 using NarakaBladepoint.Modules.Social.UI.StatusTag.ViewModels;
 using NarakaBladepoint.Modules.Social.UI.StatusTag.Views;
 
-namespace NarakaBladepoint.Modules.Social.UI.Module
+namespace NarakaBladepoint.Modules.Social.Module
 {
     /// <summary>
     /// Social模块，注册社交相关页面和ViewModel
